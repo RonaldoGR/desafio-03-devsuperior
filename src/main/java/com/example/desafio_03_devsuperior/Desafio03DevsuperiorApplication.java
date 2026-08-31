3d1ffa66f4754a03b8863dfac4b14f478d2fbb02
Ronaldo <ronaldogandrarocha@gmail.com>
2026-08-31 09:41:17 -0300

    feat: initiate project

---
+++ src/main/java/com/example/desafio_03_devsuperior/Desafio03DevsuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio_03_devsuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio03DevsuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio03DevsuperiorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafio_03_devsuperior/Desafio03DevsuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_03_devsuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio03DevsuperiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
